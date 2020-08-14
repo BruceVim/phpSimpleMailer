7783f99481fdd85272579a229c35353b0575a28f
qiuruifeng <qrfvim@163.com>
2020-08-14 18:07:16 +0800

    v1.0

--- phpSimpleMailerAutoload.php
+++ phpSimpleMailerAutoload.php
@@ -48,48 +48,4 @@ if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
     }
 }
 
-/**
- * 发送短信
- * @param $to
- * @param $title
- * @param $content
- * @param string $file_path
- * @return string
- * @throws phpmailerException
- * @Date 2020-8-13
- * @Author Bruce Vim < qrfvim@163.com >
- */
-function sendMail($to,$title,$content, $file_path='/var/www/demo/vim_test/test_email.xlsx') {
-    // 这个PHPMailer 就是之前从 Github上下载下来的那个项目
-
-    $mail = new PHPMailer;
-    //使用smtp鉴权方式发送邮件
-    $mail->isSMTP();
-    //smtp需要鉴权 这个必须是true
-    $mail->SMTPAuth = true;
-    // qq 邮箱的 smtp服务器地址，这里当然也可以写其他的 smtp服务器地址
-    $mail->Host = 'smtp.qq.com';
-    //smtp登录的账号 这里填入字符串格式的qq号即可
-    $mail->Username = 'qrfvim@qq.com';
-    // 这个就是之前得到的授权码，一共16位
-    $mail->Password = 'auxhpfrmsotnbbaa';
-    $mail->setFrom('qrfvim@qq.com', 'send_user_name');
-    // $to 为收件人的邮箱地址，如果想一次性发送向多个邮箱地址，则只需要将下面这个方法多次调用即可
-    $mail->addAddress($to);
-    // 该邮件的主题
-    $mail->Subject = $title;
-    // 该邮件的正文内容
-    $mail->Body = $content;
-
-    //附件
-    if (!empty($file_path)) $mail->addAttachment($file_path);
-
-    // 使用 send() 方法发送邮件
-    if(!$mail->send()) {
-        return '发送失败: ' . $mail->ErrorInfo;
-    } else {
-        return "发送成功";
-    }
-}
-
 
